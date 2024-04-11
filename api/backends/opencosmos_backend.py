1023f8b09283bf6b1c062cb6fee438ff444dfc6a
Toby Smith <toby.smith@open-cosmos.com>
2024-04-11 19:34:32 +0100

    add MTO request validation and attempt to get MTO search working

--- api/backends/opencosmos_backend.py
+++ api/backends/opencosmos_backend.py
@@ -1,40 +1,131 @@
+import datetime
 from email import header
 from math import prod
+from operator import ge
+from os import link
+from click import Option
+import geojson_pydantic
 from pystac import Collection
-from models import Opportunity, Product, Provider, Link
+from typing import Optional, Any
+from pydantic import BaseModel, Field
+from geojson_pydantic.features import Feature
+from api.models import Geometry, Opportunity, Product, ProductConstraints, Provider, Link
 import requests
+import json
 
 
-OC_STAC_API_URL = "https://app.open-cosmos.com/api/data/v0/stac"
+OC_STAC_API_URL = "https://test.app.open-cosmos.com/api/data/v0/stac"
+OC_MTO_URL = "https://test.app.open-cosmos.com/api/data/v1/tasking"
+
+
+class ManualTaskingSearchRequest(BaseModel):
+    instruments: list[dict[str, str]]
+    areas_of_interest: list[dict[str, str | Feature]]
+    #constraints: Optional[ProductConstraints]
+    constraints: list
+    start: datetime.datetime
+    stop: datetime.datetime
+
+    def to_json(self, **kwargs: Any):
+        return self.json(by_alias=True, exclude_unset=True, **kwargs)
 
 
 def oc_stac_collection_to_product(collection: dict) -> Product:
-    # TODO implement functionality to convert from OC STAC collection to STAT Produc
+    # Validate the STAC collection, set defaults etc
+    if "providers" not in collection:
+        collection["providers"] = [Provider(name="OpenCosmos")]
+    else:
+        collection["providers"] = [Provider.parse_obj(provider) for provider in collection["providers"]]
+    if "keywords" not in collection:
+        collection["keywords"] = []
+    if "constraints" not in collection:
+        collection["constraints"] = {}
+    if "parameters" not in collection:
+        collection["parameters"] = {}
+    if collection["links"] is None:
+        collection["links"] = []
+    else:
+        collection["links"] = [Link.parse_obj(link) for link in collection["links"]]
+
     return Product(
         id=collection["id"],
         title=collection["title"],
         description=collection["description"],
-        #constraints=constraints,
-        #parameters=parameters,
+        constraints=collection["constraints"],
+        parameters=collection["parameters"],
         license=collection["license"],
-        links=[Link.parse_obj(link) for link in collection["links"]],
+        links=collection["links"],
         keywords=collection["keywords"],
-        providers=[Provider.parse_obj(provider) for provider in collection["providers"]],
+        providers=collection["providers"],
     )
 
 
+def opportunity_to_mto_search_request(opportunity: Opportunity) -> ManualTaskingSearchRequest:
+    return ManualTaskingSearchRequest(
+        instruments=[
+            {
+                'mission_id': '55',
+                'sensor_id': 'MultiScape100 CIS'
+            }
+        ],
+        areas_of_interest=[
+            {
+                'name': 'aoi_name',
+                'geojson': Feature(geometry=opportunity.geometry, type="Feature", properties={'test': 'test'})
+            }
+        ],
+        #constraints=opportunity.constraints, #TODO implement constraints
+        constraints = list(),
+        start=opportunity.start_date,
+        stop=opportunity.end_date,
+    )
+
+
+def mto_search_response_to_opportunity_collection(response: dict, geometry: Geometry) -> list[Opportunity]:
+    opportunities = []
+    for opportunity in response["data"]:
+        opportunities.append(Opportunity(
+            id="id",
+            geometry=geometry,
+            # TODO implement constraints
+            datetime=f"{opportunity['start']}/{opportunity['stop']}",
+            product_id="hard coded opp product id",
+            constraints=None,
+            )
+        )
+    return opportunities
+
+
 class OpenCosmosBackend:
-    async def find_opportunities() -> list[Opportunity]:
-        return []
+    async def find_opportunities(self, search: Opportunity, token: str) -> list[Opportunity]:
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+
+        mto_search_response = requests.post(
+            f"{OC_MTO_URL}/search", 
+            data=json.dumps(opportunity_to_mto_search_request(search).dict(), default=str, indent=4),
+            headers=headers
+        )
+        
+        opportunities = mto_search_response_to_opportunity_collection(mto_search_response.json(), search.geometry)
+
+        return opportunities
     
     async def find_products(self, token: str) -> list[Product]:
-        products = []
         headers = {
             "Authorization": f"Bearer {token}"
         }
-        response = requests.get(f"{OC_STAC_API_URL}/collections", headers=headers)
-        print(response.json())
-        collections = response.json()
-        for collection in collections:
+
+        stac_collection_response = requests.get(
+            f"{OC_STAC_API_URL}/collections", 
+            headers=headers
+        )
+        
+        products = []
+        for collection in stac_collection_response.json():
             products.append(oc_stac_collection_to_product(collection))
+
         return products
+
